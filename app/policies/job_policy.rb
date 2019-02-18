62fbf5024ba4c80390cc3c761616269b1e0ca6c7
yaniv matalon <matalonyaniv14@gmail.com>
2019-02-18 07:40:10 -0500

    pundit added security for edit and update (only job owners can edit and update)

---
+++ app/policies/job_policy.rb
@@ -0,0 +1,33 @@
+class JobPolicy < ApplicationPolicy
+  class Scope < Scope
+    def resolve
+      scope.all
+    end
+  end
+
+  def new?
+  true
+  end
+
+  def show?
+  true
+  end
+
+  def create?
+  true
+  end
+
+  def edit?
+  record.user == user
+  end
+
+  def update?
+  record.user == user
+  end
+
+  def destroy?
+  record.user == user
+  end
+
+
+end
